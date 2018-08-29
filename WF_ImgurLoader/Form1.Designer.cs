3cfcc06c5195a1c91fd7151fa3a78c30df550586
Kirill Kalyonov <buoiiiok@yahoo.com>
2018-08-29 22:18:32 +0300

    v1.0

---
+++ WF_ImgurLoader/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WF_ImgurLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tBFilePath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tBFilePath
+            // 
+            this.tBFilePath.AllowDrop = true;
+            this.tBFilePath.Location = new System.Drawing.Point(30, 34);
+            this.tBFilePath.Multiline = true;
+            this.tBFilePath.Name = "tBFilePath";
+            this.tBFilePath.Size = new System.Drawing.Size(213, 20);
+            this.tBFilePath.TabIndex = 0;
+            this.tBFilePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.tBFilePath_DragDrop);
+            this.tBFilePath.DragEnter += new System.Windows.Forms.DragEventHandler(this.tBFilePath_DragEnter);
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(266, 32);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(75, 23);
+            this.btnPath.TabIndex = 1;
+            this.btnPath.Text = "Открыть";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(30, 96);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(213, 20);
+            this.tbUrl.TabIndex = 2;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(357, 32);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Загрузить";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Расположение файла";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ссылка на изображение";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Скопировать в буфер обмена";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 149);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.tBFilePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimpleImgur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tBFilePath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WF_ImgurLoader/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+using Imgur.API.Models;
+
+namespace WF_ImgurLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Image Files(*.BMP; *.JPG; *.GIF;*.PNG)| *.BMP; *.JPG; *.GIF;*.PNG | All files(*.*) | *.*";
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+
+            string path = filename;
+            tBFilePath.Text = path;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            var client = new ImgurClient("Client ID", "Client Secret Key"); //Client ID - add your app client id, Client Secret Key-your app secret key
+            var endpoint = new ImageEndpoint(client);			    // check https://api.imgur.com/#authentication
+            IImage image;
+            string path = tBFilePath.Text;
+            try
+            {
+
+                if (File.Exists(path))
+                {
+
+                    using (var fs = new FileStream(path, FileMode.Open))
+                    {
+                        image = endpoint.UploadImageStreamAsync(fs).GetAwaiter().GetResult();
+                    }
+                    MessageBox.Show("image upload + url\n");
+                    tbUrl.Text = image.Link;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Произошла ошибка при загрузке изображения");
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Clipboard.Clear();
+                Clipboard.SetText(tbUrl.Text);
+            }
+            catch (Exception) { MessageBox.Show("Сначала загрузите изображение"); }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] s = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string pathname in s)
+                    tBFilePath.Text += pathname;
+            }
+        }
+
+        private void tBFilePath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void tBFilePath_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] s = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string pathname in s)
+                    tBFilePath.Text += pathname;
+            }
+        }
+    }
+}
